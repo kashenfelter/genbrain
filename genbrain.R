9f5d3042b911c34306f2985d0492df6deeca479f
marcusvolz <marcus.volz@outlook.com>
2017-07-18 22:41:14 +1000

    plots restricted to square canvas

--- genbrain.R
+++ genbrain.R
@@ -1,12 +1,21 @@
 # Load packages ----
 library(ggart)
 library(tidyverse)
+library(tweenr)
 
 # Make reproducible ----
-set.seed(101)
+set.seed(10000)
 
 # Parameters ----
-n <- 1000000 # number of points
+n <- 100000000000 # number of points
+r <- 75 # circle radius
+k <- 4 # max neighbours
+X <- 10000 # canvas width
+Y <- 10000 # canvas height
+
+max_checked <- 10 # maximum times checked
+
+# Setup
 points <- data.frame(x = numeric(10), y = numeric(10), neighbours = integer(10), times_checked = integer(10))
 edges <- data.frame(x = numeric(10), y = numeric(10), xend = numeric(10), yend = numeric(10))
 
@@ -16,15 +25,17 @@ while (i <= n) {
   valid <- FALSE
   while (!valid) {
     random_point <- sample_n(points[seq(1:i-1), ] %>%
-                               filter(neighbours <= 4, times_checked <= 10) %>%
-                               top_n(max(nrow(.) / 2, 10), times_checked),
-                             1) # Pick a point at random
+                               filter(neighbours <= k, times_checked <= max_checked) %>%
+                               top_n(max(floor(nrow(.) / 2), 10), times_checked), 1) # Pick a point at random
     alpha <- runif(1, -2 * pi, 2 * pi) # Pick a random direction
-    v <- c(cos(alpha), sin(alpha)) # Create directional vector
-    xj <- random_point$x[1] + v[1] * 2
-    yj <- random_point$y[1] + v[2] * 2
+    v <- c(cos(alpha), sin(alpha)) * r # Create directional vector
+    xj <- random_point$x[1] + v[1]
+    yj <- random_point$y[1] + v[2]
+    if(xj < -X/2 | xj > X/2 | yj < -Y/2 | yj > Y/2) {
+      next
+    }
     points_dist <- points %>% mutate(d = sqrt((xj - x)^2 + (yj - y)^2))
-    if (min(points_dist$d) >= 2) {
+    if (min(points_dist$d) >= 1 * r) {
       points[i, ] <- c(xj, yj, 1, 0)
       edges[i, ] <- c(xj, yj, random_point$x[1], random_point$y[1])
       valid <- TRUE
@@ -39,15 +50,30 @@ while (i <= n) {
   print(i)
 }
 
+delta <- 50
+edges2 <- edges %>%
+  rowwise() %>%
+  mutate(x = x + runif(1, -delta, delta),
+         y = y + runif(1, -delta, delta),
+         xend = xend + runif(1, -delta, delta),
+         yend = yend + runif(1, -delta, delta))
+
+df <- list(edges, edges2)
+
+tf <- tween_states(df, tweenlength = 2, statelength = 1,
+                   ease = "linear",
+                   nframes = 500)
+
 # Make plot ----
 p <- ggplot() +
   geom_segment(aes(x, y, xend = xend, yend = yend),
-               edges %>% filter(x != xend), lineend = "round", size = 0.5) +
+               edges %>% filter(x != xend), lineend = "round", size = 0.9, alpha = 1) +
   coord_equal() +
-  theme_blankcanvas() #+
-  #theme(plot.margin = unit(c(10, 10, 10, 10), units = "mm")) # top, right, bottom, and left margins
+  xlim(-5000, 5000) +
+  ylim(-5000, 5000) +
+  theme_blankcanvas(margin_cm = 0)
 
 #Save plot ----
-ggsave("plots/genbrain004.tiff", width = 63.5, height = 61, units = "cm", dpi = 720)
-# saveRDS(edges, "edges-2.RDS")
+ggsave("plots/plot009.png", width = 20, height = 20, units = "in", dpi = 300)
+ saveRDS(edges, "edges-3.RDS")
 # edges <- readRDS("edges.RDS")
